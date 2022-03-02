b27508eb30299d4e7ef581a975c86463691e7668
gizem <gizemc.deniz@gmail.com>
2022-03-02 20:24:47 +0300

    footer and navbar add

---
+++ src/components/ActorPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ActorPage() {
+  return (
+    <div>ActorPage</div>
+  )
+}
+
+export default ActorPage
---
+++ src/containers/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavBar from '../components/NavBar'
+import Footer from '../components/Footer'
+
+function HomePage() {
+  return (
+    <div>
+         <NavBar/>
+         <Footer/>
+    </div>
+  )
+}
+
+export default HomePage
