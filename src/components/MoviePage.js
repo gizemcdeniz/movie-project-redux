7a9840a387f08cc2c3640d3fcd163972c3cd06db
gizem <gizemc.deniz@gmail.com>
2022-03-09 20:38:28 +0300

    movie fetch and movie page style

--- src/components/MoviePage.js
+++ src/components/MoviePage.js
@@ -1,35 +1,50 @@
 import React, { useEffect, useState } from "react";
+import { Card, Button, Container} from 'react-bootstrap';
+import './movie.css';
 
 function MoviePage() {
-    const [allMovie, setallMovie] = useState([]);
+    const [allMovie, setAllMovie] = useState([]);
+
+    // useEffect(() => {
+    //     const url = "https://api.themoviedb.org/3/list/1?api_key=d1793e2d14633816edae12bcc4e69e85&language=en-US";
+
+    //     const fetchData = async () => {
+    //         try {
+    //             const response = await fetch(url);
+    //             const json = await response.json();
+    //             console.log(json);
+    //             setallMovie(json);
+    //             console.log(allMovie)
+    //         } catch (error) {
+    //             console.log("error", error);
+    //         }
+    //     };
+
+    const URL = "https://api.themoviedb.org/3/list/1?api_key=d1793e2d14633816edae12bcc4e69e85&language=en-US"
 
     useEffect(() => {
-        const url = "https://api.themoviedb.org/3/list/1?api_key=d1793e2d14633816edae12bcc4e69e85&language=en-US";
-
-        const fetchData = async () => {
-            try {
-                const response = await fetch(url);
-                const json = await response.json();
-                console.log(json);
-                setallMovie(json);
-                console.log(allMovie)
-            } catch (error) {
-                console.log("error", error);
-            }
-        };
-
-        fetchData();
-    }, []);
+        fetch(URL)
+            .then(res => res.json())
+            .then(data => setAllMovie(data.items))
+    }, [URL])
 
     return (
-        <div>
-            <p>Hello from moviePage</p>
-            {/* <ul>
-                {allMovie.map(movie => {
-                    <li key={movie.id}>{movie.title}</li>
-                }}
-        
-        </ul> */}
+        <div className="moviePage">
+            <Container  style={{ border: '1px solid blue', display:'flex', flexWrap: 'wrap', justifyContent: 'center' }} >
+                {allMovie && allMovie.map(movie => (
+                            <Card className='image-container d-flex justify-content-start m-3' style={{ width: '20%', padding: '3em' }}>
+                                <Card.Img style={{ width: '10em' }} variant="top" src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt="posterImage" />
+                                <Card.Body>
+                                    <Card.Title>{movie.title}</Card.Title>
+                                    <Card.Text> Vote Average:
+                                        {movie.vote_average}
+                                    </Card.Text>
+                                    <Button variant="primary">Go somewhere</Button>
+                                </Card.Body>
+                            </Card>
+
+                ))}
+            </Container>
         </div>
     )
 }
